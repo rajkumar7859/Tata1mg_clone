70621e1971add4f2f2ce7ac70b52d8058a7c29cc
rajkumar7859 <rajkumarvishwakarma8759@gmail.com>
2022-08-27 13:11:19 +0530

    some bug fix

--- tata_1mg_clone_project/src/ProductsPage/Main_product_page.jsx
+++ tata_1mg_clone_project/src/ProductsPage/Main_product_page.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Navbar_dropdown } from "../Navbar/Components/Navbar_dropdown";
 import { Navbar_search } from "../Navbar/Components/Navbar_search";
 import Products from "./Products";
-import Sidebar from "./SideBar/Sidebar";
 
 export const Main_product_page = () => {
   return (
